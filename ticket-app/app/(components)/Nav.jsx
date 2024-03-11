f3af78e6ab34f80fca7ed1c6dd1b627b373d8674
fb0801 <fbhatti0801@gmail.com>
2024-03-11 18:19:40 +0000

    nav styling and more

--- ticket-app/app/(components)/Nav.jsx
+++ ticket-app/app/(components)/Nav.jsx
@@ -1,8 +1,25 @@
+import Link from 'next/link'
 import React from 'react'
+
+import { faHome, faTicket } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 
 const Nav = () => {
   return <nav>
-    
+    <div>
+        <Link href="/">
+            <FontAwesomeIcon icon={faHome} className='icon'/>
+        </Link>
+
+        <Link href="/TicketPage/new">
+            <FontAwesomeIcon icon={faTicket} className='icon'/>
+        </Link>
+
+    </div>
+    <div>
+        <p className='text-default-text'>Email</p>
+    </div>
   </nav>
 }
 
