b09c754ff2a5afad3ee0a351845b958c9bdb2ddc
fb0801 <fbhatti0801@gmail.com>
2024-03-12 17:13:46 +0000

    components 5

--- ticket-app/app/(components)/TicketCard.jsx
+++ ticket-app/app/(components)/TicketCard.jsx
@@ -5,13 +5,21 @@ import ProgressDisplay from './ProgressDisplay'
 import StatusDisplay from './StatusDisplay'
 
 const TicketCard = () => {
-  return 
+  return (
   <div className='flex flex-col bg-card hover:bg-card-hover rounded-md shadow-lg p-3 m-2'>
+    <div className='flex mb-3'>
+      <PriorityDisplay />
+<div className='ml-auto'>
     <DeleteBlock />
-  <PriorityDisplay />
+    </div>
+    </div>
+    <h4>Ticket Title</h4>
+    <hr className='h-px border-0 bg-page mb-2'/>
+    <p className='whitespace-pre-wrap'></p>
   <ProgressDisplay />
   <StatusDisplay />
   </div>
+  )
 }
 
 export default TicketCard
