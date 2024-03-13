31cdfe2b2fc96899ff93410a3beb41f9f8e93800
fb0801 <fbhatti0801@gmail.com>
2024-03-13 16:59:10 +0000

    creating the form

---
+++ ticket-app/app/(components)/EditTicketForm.jsx
@@ -0,0 +1,23 @@
+"use client";
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+
+
+const EditTicketForm = () => {
+
+    const startingTicketData = {
+        title: "",
+        description: "",
+        priority: 1,
+        progress: 0,
+        status: "not started",
+        category: "Hardware Problem",
+      };
+
+      const [formData, setFormData]
+  return 
+    <div> TicketForm   </div>
+  
+}
+
+export default EditTicketForm
