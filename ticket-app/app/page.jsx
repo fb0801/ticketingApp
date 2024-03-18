aa7709e3a16db1c4ec4f7826faab55c820abe286
fb0801 <fbhatti0801@gmail.com>
2024-03-18 17:31:40 +0000

    display the tickets and more 2

--- ticket-app/app/page.jsx
+++ ticket-app/app/page.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import TicketCard from './(components)/TicketCard'
+import { set } from 'mongoose';
 
 const getTickets = async () => {
   try {
@@ -17,18 +18,35 @@ const getTickets = async () => {
   }
 };
 
-const Dashboard = () => {
+const Dashboard = async () => {
+
+  const data = await getTickets();
+
+  const uniqeCategories = [
+    ... new set(tickets?.map(({category}) => category)),
+  ]
+
   return (
-  <div className='p-5'> 
-  <div className='lg:grid grid-cols-2 xl:grid-cols-4'>
-   
-    <TicketCard/>
-    <TicketCard/>
-    <TicketCard/>
-    <TicketCard/>
+    <div className="p-5">
+    <div>
+      {tickets &&
+        uniqueCategories?.map((uniqueCategory, categoryIndex) => (
+          <div key={categoryIndex} className="mb-4">
+            <h2>{uniqueCategory}</h2>
+            <div className="lg:grid grid-cols-2 xl:grid-cols-4 ">
+              {tickets.filter((ticket) => ticket.category === uniqueCategory).map((filteredTicket, _index) => (
+                  <TicketCard
+                    id={_index}
+                    key={_index}
+                    ticket={filteredTicket}
+                  />
+                ))}
+            </div>
+          </div>
+        ))}
     </div>
-    </div> 
-  )
-}
+  </div>
+);
+};
 
 export default Dashboard
