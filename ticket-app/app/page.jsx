5c5efa1839b23cf60ab5ba1c117ed2f58c233a70
fb0801 <fbhatti0801@gmail.com>
2024-03-18 17:26:08 +0000

    display the tickets and more

--- ticket-app/app/page.jsx
+++ ticket-app/app/page.jsx
@@ -1,6 +1,22 @@
 import React from 'react'
 import TicketCard from './(components)/TicketCard'
 
+const getTickets = async () => {
+  try {
+    const res = await fetch("http://localhost:3000/api/Tickets", {
+      cache: "no-store",
+    });
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch topics");
+    }
+
+    return res.json();
+  } catch (error) {
+    console.log("Error loading topics: ", error);
+  }
+};
+
 const Dashboard = () => {
   return (
   <div className='p-5'> 
