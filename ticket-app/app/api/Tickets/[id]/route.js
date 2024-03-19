7ca0bfed073858752c043e23797bcf2efdfeb670
fb0801 <fbhatti0801@gmail.com>
2024-03-19 17:23:53 +0000

    update ticket 2

--- ticket-app/app/api/Tickets/[id]/route.js
+++ ticket-app/app/api/Tickets/[id]/route.js
@@ -2,6 +2,12 @@
 import Ticket from "@/app/models/Ticket";
 import { NextResponse } from "next/server";
 
+export async function GET(request, { params }) {
+  const { id } = params;
+
+  const foundTicket = await Ticket.findOne({ _id: id });
+  return NextResponse.json({ foundTicket }, { status: 200 });
+}
 
 
 export async function DELETE(req, { params }) {
